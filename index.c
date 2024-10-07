4ea85cadd3a4e48703c5470063475ac833273391
vinit <vinitmp.promail@gmail.com>
2024-10-07 09:24:34 +0530

    - write initial function

---
+++ index.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define LIST_SIZE 1000
+#define MAX_NUMBER 10000
+
+int main(){
+// Writing  various sorting alogoriths in c to check for it running time, 
+// So that we get a better perspective at it.
+//
+//
+//
+
+
+int numbers[LIST_SIZE];
+srand(time(NULL));
+
+
+
+for(int i= 0; i<LIST_SIZE;i++){
+    
+}
+
+
+
+
+
+
+
+}
